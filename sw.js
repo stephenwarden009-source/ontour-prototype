47fc4b9695e3dcdf62dce1da69d875ba800db7f3
stephenwarden009-source <stephenwarden009@gmail.com>
2026-04-20 11:08:41 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = 'ontour-v1';
+const BASE = self.location.pathname.replace(/\/sw\.js$/, '');
+
+const ASSETS = [
+  `${BASE}/`,
+  `${BASE}/index.html`,
+  `${BASE}/manifest.json`
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match(`${BASE}/index.html`));
+    })
+  );
+});
